46a93e2e77508bfde82c00f6c3be9d1de7904f91
HardyChina <1059275201@qq.com>
2019-08-14 17:35:52 +0800

    完成链表的删除

--- src/part3/Test18.java
+++ src/part3/Test18.java
@@ -1,7 +1,7 @@
 package part3;
 
 /**
- * 在O（1）时间内完成链表的删除
+ * 链表的删除
  * @author 10592
  */
 public class Test18 {
@@ -13,7 +13,7 @@ public class Test18 {
     public void deleteNode(ListNode head, ListNode target){
         // 当target不为尾节点时
         if (target.next != null){
-            target.value = target.next.value;
+            target.val = target.next.val;
             target.next = target.next.next;
             target.next.next = null;
             System.gc();
@@ -34,8 +34,55 @@ public class Test18 {
 
     }
 
+    /**
+     * 删除重复的节点
+     * @param head 头结点
+     * @return
+     */
+    public static ListNode deleteDuplication(ListNode head) {
+        if (head == null){
+            throw new IllegalArgumentException("头结点不能为空");
+        }
+        ListNode preNode = null;
+        ListNode node = head;
+        while (node != null) {
+            boolean needDel = false;
+            ListNode next = node.next;
+            // 当存在重复时删除，使preNode指向后边不重复的元素
+            if (next != null && node.val == next.val) {
+                needDel = true;
+            }
+            if (!needDel) {
+                preNode = node;
+                node = next;
+            } else {
+                int value = node.val;
+                while (next!=null && next.val == value) {
+                   next = next.next;
+                }
+                if (preNode == null){
+                    head = next;
+                }
+                else {
+                    preNode.next = next;
+                }
+                node = next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode next  = new ListNode(1);
+        head.next = next;
+        System.out.println(deleteDuplication(head));
+    }
 }
 class ListNode{
-    int value;
+    int val;
     ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
 }
