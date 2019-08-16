3148b67f0d29673958fe8543759762ddd9961cd7
HardyChina <1059275201@qq.com>
2019-08-16 17:22:02 +0800

    添加反转链表的递归实现

--- src/part3/Test24.java
+++ src/part3/Test24.java
@@ -6,7 +6,7 @@ package part3;
  */
 public class Test24 {
     /**
-     *
+     *反转链表，需要同时取得三个节点，要注意的是节点初始化，以及注意反转后原来的head.next为null
      * @param head
      * @return
      */
@@ -30,6 +30,24 @@ public class Test24 {
         return reverseList;
     }
 
+    /**
+     * 递归实现
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        // 得到尾节点
+        ListNode reverseList = reverseList1(head.next);
+        // 当前节点的后一个节点指向自己
+        head.next.next = head;
+        // 当前节点的next节点不再指向原来后边的节点，最后原来头节点的next为空
+        head.next = null;
+        return reverseList;
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         ListNode l2 = new ListNode(2);
@@ -38,6 +56,7 @@ public class Test24 {
         head.next = l2;
         l2.next = l3;
         l3.next = l4;
-        System.out.println(reverseList(head).val);
+       // System.out.println(reverseList(head).val);
+        System.out.println(reverseList1(head).val);
     }
 }
